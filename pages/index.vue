d854c0d6adbe831f444194c0d5045bc999bd85fb
kimminseo <kms326464@naver.com>
2025-04-15 20:08:13 +0900

    setup

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex justify-center">
+    <h1>
+      HELLO! I'M COMET-MS
+    </h1>
+    <h2>
+      Welcome to my portfolio!
+    </h2>
+  </div>
+
+  <div>
+    <div class="">
+      Designing systems. Delivering experiences.
+    </div>
+    <div>
+      A full-stack developer who values clarity and craft.
+    </div>
+  </div>
+
+  <div>
+    <div>
+      Skills
+    </div>
+    <div>
+      Strengths
+    </div>
+  </div>
+
+  <div>
+    <h1>
+      Which of my works are you curious about?
+    </h1>
+    <UButton label="Instagram clone" />
+    <UButton label="Real time chat" />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+body {
+  background: linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url("public/background-image.jpg");
+}
+</style>
+
